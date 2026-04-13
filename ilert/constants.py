0399ac422f39f7848d8aeee71073c4873bab469f
Tim Nguyen Van <tim@ilert.com>
2026-04-13 17:42:54 +0200

    dev

--- ilert/constants.py
+++ ilert/constants.py
@@ -4,4 +4,4 @@ few constants shared by all the module of the integration
 """
 
 # https://docs.ilert.com/developer-docs/rest-api/api-reference/events
-DEFAULT_EVENTS_URL = "https://api.ilert.com/api/v1/events/sekoia"
+DEFAULT_EVENTS_URL = "https://api.ilert.dev/api/v1/events/sekoia"
